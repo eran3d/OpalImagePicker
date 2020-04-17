2cf99f6e04cc9e7ee1d52c598c7f7611763766c5
Eran Jalink <jalink@datlinq.com>
2020-04-17 18:15:09 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
-    name: "OpalImagePicker"
+    name: "OpalImagePicker",
+    products: [
+        .library(name: "OpalImagePicker", targets: ["OpalImagePicker"]),
+    ],
+
+    targets: [
+        .target(
+            name: "OpalImagePicker",
+            dependencies: [],
+            path: "OpalImagePicker"
+            ),
+        .testTarget(
+            name: "OpalImagePickerTests",
+            dependencies: ["OpalImagePicker"]),
+    ]
 )
